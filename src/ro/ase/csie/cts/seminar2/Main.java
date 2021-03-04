a237fe3cdfd48802642043838f47f679f9df1e6d
Diana <dianasuica99@gmail.com>
2021-03-04 15:14:00 +0200

    added bank account

--- src/ro/ase/csie/cts/seminar2/Main.java
+++ src/ro/ase/csie/cts/seminar2/Main.java
@@ -1,19 +1,33 @@
 package ro.ase.csie.cts.seminar2;
 
+import ro.ase.csie.cts.seminar2.solid.BankAccount;
+import ro.ase.csie.cts.seminar2.solid.Persoana;
+
 public class Main {
 
 	public static void main(String[] args) {
-        CalendarUtil cu = new CalendarUtil();
 		
-		try {
-			System.out.println(cu.weekdDay(4));
-			System.out.println(cu.weekDay2(3));
-			System.out.println(cu.weekDay3(3));
-			System.out.println(cu.weekDay4(7));
-		} catch (IncorectDayException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
+//        CalendarUtil cu = new CalendarUtil();
+//        
+//       
+//		
+//		try {
+//			System.out.println(cu.weekdDay(4));
+//			System.out.println(cu.weekDay2(3));
+//			System.out.println(cu.weekDay3(3));
+//			System.out.println(cu.weekDay4(7));
+//		} catch (IncorectDayException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
+		Persoana p = new Persoana("Diana");
+		BankAccount account = new BankAccount("INGB123456789",p);
+		account.deposit(100);
+		account.withdraw(50);
+		account.withdraw(70);
+		System.out.println(account.getBalance());
 
 
 	}
---
+++ src/ro/ase/csie/cts/seminar2/solid/BankAccount.java
@@ -0,0 +1,44 @@
+package ro.ase.csie.cts.seminar2.solid;
+
+public class BankAccount {
+	
+	private String iban;
+	private long balance;
+	private Persoana accountHolder;
+	
+	public BankAccount(String iban, Persoana persoana) {
+		this.iban = iban;
+		this.accountHolder = persoana;
+		this.balance = 0;
+	}
+	
+	public void withdraw(long amount) {
+		System.out.println("withdrawing " + amount + " from " + iban);
+		balance -= amount;
+	}
+	
+	public void deposit(long amount) {
+		System.out.println("depositing " + amount + " in " + iban);
+		balance += amount;
+		
+	}
+	
+	public String getIban() {
+		return iban;
+	}
+
+
+	public long getBalance() {
+		return balance;
+	}
+
+
+	public Persoana getAccountHolder() {
+		return accountHolder;
+	}
+
+
+	
+	
+	
+}
