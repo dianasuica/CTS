a31d46dbfde48a8fb3db65ffd08d011b8c4f0419
Diana <dianasuica99@gmail.com>
2021-05-12 14:25:10 +0300

    adaugare manager command inainte de pauza

--- src/ro/ase/csie/cts/seminar10/command/TestCommand.java
+++ src/ro/ase/csie/cts/seminar10/command/TestCommand.java
@@ -6,8 +6,14 @@ public class TestCommand {
 		TaskAsincron autoSave = new TaskToDo(new ModulAutosave(), "Autosave",5);
 		TaskAsincron autoUpdate = new TaskToDo(new ModulUpdateClient(), "Update v2", 3);
 		
-		autoSave.executaTask();
-		autoUpdate.executaTask();
+		ManagerTaskuri manager = new ManagerTaskuri();
+		manager.adaugaTask(autoSave);
+		manager.adaugaTask(autoUpdate);
+		
+		System.out.println("Jocul este activ");
+		
+		manager.executaTask();
+		manager.executaTask();
 	}
 
 }
